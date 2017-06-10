58924172b4e92a16c8da59392842c56ebe4da7de
Bolaji Olajide <bolaji.olajide@andela.com>
2017-06-10 12:41:30 +0100

    group nested import properly

--- app/decorators.py
+++ app/decorators.py
@@ -3,10 +3,17 @@ Define decorators for the application.
 
 All decorators to be used in the application are defined here.
 """
-
+from datetime import timedelta
 import functools
 
-from flask import jsonify, wrappers, request, url_for, current_app
+from flask import (
+    jsonify,
+    wrappers,
+    request,
+    url_for,
+    current_app,
+    make_response
+)
 
 
 def json(f):
@@ -97,4 +104,46 @@ def paginate():
                 'bucketlists': [each.to_json() for each in content]
             })
         return wrapped
+    return decorator
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return functools.update_wrapper(wrapped_function, f)
     return decorator
